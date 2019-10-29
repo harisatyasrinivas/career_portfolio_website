06e0e07d6906ca4b1176a0ec5202907e7a745053
harisatyasrinivas <satyasrinivas1234@gmail.com>
2019-10-29 10:39:43 -0400

    Models Creation Successful

---
+++ portfolio/src/main/java/com/satya/career/portfolio/models/Address.java
@@ -0,0 +1,12 @@
+package com.satya.career.portfolio.models;
+
+public class Address {
+	
+	private String addressLine1;
+	private String addressLine2;
+	private String state;
+	private String province;
+	private String zipcode;
+	
+
+}
